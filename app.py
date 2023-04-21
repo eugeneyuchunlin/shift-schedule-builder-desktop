5a5bdafd33affda393ed769be8afda3393cf8e6c
Eugene <lin.eugene.l.e@gmail.com>
2023-04-21 14:46:21 +0800

    Enable the table's size to automatically update
    Add seperator to the File menu bar

--- app.py
+++ app.py
@@ -11,48 +11,35 @@ from PySide6.QtCore import Qt
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
 
-class SchedulingResultTable(QTableWidget):
+import calendar
 
-    def __init__(self):
+class ShiftTable(QTableWidget):
+
+    def __init__(self, empty_table=True, number_of_rows=None, year=None, month=None):
         super().__init__()
 
-        self.initUI()
+    def createShiftTable(self, number_of_people, year, month):
+        days = calendar.monthrange(year, month)[1] # Handle the exception !!
+
+        self.setRowCount(number_of_people)
+        self.setColumnCount(days)
+        self.setHorizontalHeaderLabels([str(i) for i in range(1, days+1)])
+        self.setVerticalHeaderLabels([str(i) for i in range(1, number_of_people + 1)])
+        self.resizeColumnsToContents()
+
+
+    def loadShiftTable(self):
         pass
 
-    def initUI(self):
-        self.setRowCount(10)
-        self.setColumnCount(31)
-        self.setHorizontalHeaderLabels([str(i) for i in range(1, 31)])
-        self.setVerticalHeaderLabels([str(i) for i in range(1, 11)])
-
-        # Set some data in the table
-
-
-        self.setItem(0, 0, QTableWidgetItem("Cell 1"))
-        self.setItem(0, 1, QTableWidgetItem("Cell 2"))
-        self.setItem(0, 2, QTableWidgetItem("Cell 3"))
-
-        self.setItem(1, 0, QTableWidgetItem("Cell 4"))
-        self.setItem(1, 1, QTableWidgetItem("Cell 5"))
-        self.setItem(1, 2, QTableWidgetItem("Cell 6"))
-
-        self.setItem(2, 0, QTableWidgetItem("Cell 7"))
-        self.setItem(2, 1, QTableWidgetItem("Cell 8"))
-        self.setItem(2, 2, QTableWidgetItem("Cell 9"))
-
-        self.setItem(3, 0, QTableWidgetItem("Cell 10"))
-        self.setItem(3, 1, QTableWidgetItem("Cell 11"))
-        self.setItem(3, 2, QTableWidgetItem("Cell 12"))
-
-        # Resize the columns to fit the data
-        self.resizeColumnsToContents()
+    def setShiftTable(self):
         pass
 
 class Configuration(QWidget):
 
-    def __init__(self):
+    def __init__(self, table:ShiftTable):
         super().__init__()
         self.initUI()
+        self._table = table
 
     def initUI(self):
 #        per_grave = 10 #m1
@@ -70,60 +57,83 @@ class Configuration(QWidget):
 
         formlayout = QFormLayout()
 
-        per_grave_label = QLabel("Pergrave")
-        per_grave_edit = QLineEdit("10")
-        formlayout.addRow(per_grave_label, per_grave_edit)
+        self.per_grave_label = QLabel("Pergrave")
+        self.per_grave_edit = QLineEdit()
+        formlayout.addRow(self.per_grave_label, self.per_grave_edit)
+        self.per_grave_edit.editingFinished.connect(self._generateNewMonthTable)
 
 
-        n1_label = QLabel("n1")
-        n1_edit = QLineEdit("")
-        formlayout.addRow(n1_label, n1_edit)
+        self.n1_label = QLabel("n1")
+        self.n1_edit = QLineEdit("")
+        formlayout.addRow(self.n1_label, self.n1_edit)
 
 
-        k_label = QLabel("k")
-        k_edit = QLineEdit("")
-        formlayout.addRow(k_label, k_edit)
+        self.k_label = QLabel("k")
+        self.k_edit = QLineEdit("")
+        formlayout.addRow(self.k_label, self.k_edit)
 
-        num_sweeps_label = QLabel("num_sweeps")
-        num_sweeps_edit = QLineEdit("")
-        formlayout.addRow(num_sweeps_label, num_sweeps_edit)
+        self.num_sweeps_label = QLabel("num_sweeps")
+        self.num_sweeps_edit = QLineEdit("")
+        formlayout.addRow(self.num_sweeps_label, self.num_sweeps_edit)
 
-        year_label = QLabel("year")
-        year_edit = QLineEdit("")
-        formlayout.addRow(year_label, year_edit)
-
-        month_label = QLabel("month")
-        month_edit = QLineEdit("")
-        formlayout.addRow(month_label, month_edit)
-
-        lmda_label = QLabel("lmda")
-        lmda_edit = QLineEdit("")
-        formlayout.addRow(lmda_label, lmda_edit)
-
-        lmdb_label = QLabel("lmdb")
-        lmdb_edit = QLineEdit("")
-        formlayout.addRow(lmdb_label, lmdb_edit)
+        self.year_label = QLabel("year")
+        self.year_edit = QLineEdit("")
+        formlayout.addRow(self.year_label, self.year_edit)
+        self.year_edit.editingFinished.connect(self._generateNewMonthTable)
 
 
-        lmdc_label = QLabel("lmdc")
-        lmdc_edit = QLineEdit("")
-        formlayout.addRow(lmdc_label, lmdc_edit)
+        self.month_label = QLabel("month")
+        self.month_edit = QLineEdit("")
+        formlayout.addRow(self.month_label, self.month_edit)
+        self.month_edit.editingFinished.connect(self._generateNewMonthTable)
 
-        lmde_label = QLabel("lmde")
-        lmde_edit = QLineEdit("")
-        formlayout.addRow(lmde_label, lmde_edit)
+        self.lmda_label = QLabel("lmda")
+        self.lmda_edit = QLineEdit("")
+        formlayout.addRow(self.lmda_label, self.lmda_edit)
 
-        lmdd_label = QLabel("lmdd")
-        lmdd_edit = QLineEdit("")
-        formlayout.addRow(lmdd_label, lmdd_edit)
+        self.lmdb_label = QLabel("lmdb")
+        self.lmdb_edit = QLineEdit("")
+        formlayout.addRow(self.lmdb_label, self.lmdb_edit)
+
+
+        self.lmdc_label = QLabel("lmdc")
+        self.lmdc_edit = QLineEdit("")
+        formlayout.addRow(self.lmdc_label, self.lmdc_edit)
+
+        self.lmde_label = QLabel("lmde")
+        self.lmde_edit = QLineEdit("")
+        formlayout.addRow(self.lmde_label, self.lmde_edit)
+
+        self.lmdd_label = QLabel("lmdd")
+        self.lmdd_edit = QLineEdit("")
+        formlayout.addRow(self.lmdd_label, self.lmdd_edit)
 
         runbutton = QPushButton("Run")
         formlayout.addRow(runbutton)
 
-        # formlayout.setAlignment(Qt.AlignmentFlag.AlignRight)
-
         self.setLayout(formlayout)
 
+    def _generateNewMonthTable(self):
+        # check data
+        year = self.year_edit.text()
+        month = self.month_edit.text()
+        number_of_people = self.per_grave_edit.text()
+
+        if year.isdigit() and len(year) > 0:
+            year = int(year)
+        else:
+            return
+        if month.isdigit() and len(month) > 0:
+            month = int(month)
+        else:
+            return
+        if number_of_people.isdigit() and len(number_of_people) > 0:
+            number_of_people = int(number_of_people)
+        else:
+            return
+
+        self._table.createShiftTable(number_of_people, year, month)
+        pass
 
 class ConfigurationAndResult(QWidget):
     def __init__(self, name):
@@ -136,16 +146,21 @@ class ConfigurationAndResult(QWidget):
     def initUI(self):
 
         layout = QGridLayout()
-        self.form = Configuration()
-
-        self.table = SchedulingResultTable()
-
+        self.table = ShiftTable()
+        self.form = Configuration(self.table)
 
         layout.setColumnStretch(0, 2)
         layout.addWidget(self.form,  0, 1)
         layout.addWidget(self.table, 0, 0)
         self.setLayout(layout)
-        pass
+
+    def setMonth(self, month):
+        self._month = month
+
+    def setYear(self, year):
+        self._year = year
+
+
 
 class SchedulingConfiguration(QWidget):
     def __init__(self):
@@ -165,7 +180,6 @@ class SchedulingConfiguration(QWidget):
         vbox.addWidget(self.tabwidget)
 
         self.setLayout(vbox)
-        self.setWindowTitle("Form Widget")
 
 
     def addANewTab(self):
@@ -176,8 +190,10 @@ class SchedulingConfiguration(QWidget):
 
     def createATab(self, name):
         tab = ConfigurationAndResult(name)
-#        tab = MyWidget(name)
         return tab
+
+    def switchToLastTab(self):
+        self.tabwidget.setCurrentIndex(self.number_of_untitled_tabs - 1)
 
 
 class MainWindow(QMainWindow):
@@ -186,27 +202,39 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        configuration = SchedulingConfiguration()
-        self.setCentralWidget(configuration)
+        self.configuration = SchedulingConfiguration()
+        self.setCentralWidget(self.configuration)
 
         menuBar = self.menuBar()
         fileMenu = menuBar.addMenu("File")
         new_action = QAction("New", self)
         new_action.setShortcut("Ctrl+t")
-        new_action.triggered.connect(configuration.addANewTab)
+        new_action.triggered.connect(self.newTabTrigger)
 
 
         open_action = QAction("Open", self)
 
-        fileMenu.addActions([new_action, open_action])
 
         saveMenu = QMenu("Save")
         save_all_action = QAction("Save All", self)
         save_action = QAction("Save", self)
 
+
+
+        seperator_action = QAction("", self)
+        seperator_action.setSeparator(True)
+        export = QAction("Export", self)
+
+
+
+        fileMenu.addActions([new_action, open_action])
         saveMenu.addActions([save_all_action, save_action])
         fileMenu.addMenu(saveMenu)
+        fileMenu.addActions([seperator_action, export])
 
+    def newTabTrigger(self):
+        self.configuration.addANewTab()
+        self.configuration.switchToLastTab()
 
 
 
