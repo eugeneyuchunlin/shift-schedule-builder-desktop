9f4bf7d25ad378dffd6765f7a86a8fcba1e1fd9b
Eugene <lin.eugene.l.e@gmail.com>
2023-10-18 15:30:02 -0500

    Add a tiny server module

---
+++ src/server/route.py
@@ -0,0 +1,155 @@
+import socket
+import json
+import struct
+
+class Response(object):
+
+    def __init__(self, client_socket:socket.socket):
+        self._client_socket = client_socket
+        pass
+    
+    def close(self):
+        self._client_socket.close() 
+
+    def socket(self):
+        return self._client_socket
+
+class HttpResponse(Response):
+    def __init__(self, client_socket:socket.socket):
+        self._client_socket = client_socket
+        pass
+
+    def send(self, code, message, content_type="text/html"):
+        
+        response = f"HTTP/1.1 {code}\r\n"
+        response += f"Content-Type: {content_type}\r\n"
+        response += "Connection: close\r\n"
+        response += f"Content-Length: {len(message)}\r\n"
+        response += "\r\n"
+        response += message + "\r\n"
+
+        self._client_socket.send(response.encode('utf-8'))
+        self._client_socket.close()
+
+
+class WebSocketResponse(Response):
+
+    def send(self, message):
+        response_data = struct.pack('B', 0x81) + struct.pack('B', len(message)) + message.encode('utf-8')
+        self._client_socket.send(response_data)
+
+class Request(object):
+
+    def __init__(self, client_socket: socket.socket):
+        self._client_socket = client_socket
+
+        # get the HTTP request
+        request = self._client_socket.recv(2048).decode('utf-8')
+        request_lines = request.split('\r\n')  # Split the request into lines
+
+        # Parse the request line to get method, URI, and protocol
+        request_line_parts = request_lines[0].split(' ')
+        self.method = request_line_parts[0]
+        self.uri = request_line_parts[1]
+        self.protocol = request_line_parts[2]
+        self.request = request
+
+        self.headers = {}
+        self.body = ""
+
+        # Parse headers and body
+        for line in request_lines[1:]:
+            if line:
+                header_name, header_value = line.split(': ', 1)
+                self.headers[header_name] = header_value
+            else:
+                # An empty line marks the end of headers and the start of the body
+                self.body = '\r\n'.join(request_lines[request_lines.index('') + 1:])
+                # print("body : ", self.body)
+                break
+
+    def socket(self):
+        return self._client_socket
+
+
+
+class Route(object):
+
+    def __init__(self, request: Request, response: Response):
+        self.request = request
+        self.response = response
+        pass
+    
+    def handler(self):
+        pass
+
+
+class WebSocketRoute(Route):
+
+    def __init__(self, request: Request, response: WebSocketResponse):
+
+        self.request = request
+        self.response = response
+        self._client_socket = request.socket()
+
+    def _decode_websocket_data(self, data):
+        opcode = data[0] & 0x0F
+        payload_length = data[1] & 0x7F
+        mask_key = data[2:6]
+        encoded_data = data[6:]
+
+        decoded_data = bytearray(len(encoded_data))
+        for i in range(len(encoded_data)):
+            decoded_data[i] = encoded_data[i] ^ mask_key[i % 4]
+
+        return opcode, decoded_data
+
+    def _recv(self, size=2048):
+        try:
+            data = self._client_socket.recv(size)
+            print(data)
+            if len(data) == 0:
+                self.close()
+                return None
+
+            opcode, decoded_data = self._decode_websocket_data(data)
+
+            if opcode == 1:
+                message = decoded_data.decode('utf-8')
+                return message
+            elif opcode == 8:
+                response_data = struct.pack('B', 0x88) + struct.pack('B', 0)
+                self._client_socket.send(response_data)
+                self.close()
+                return None
+        except ConnectionResetError:
+            self.close()
+        
+
+    def close(self):
+        self._client_socket.close()
+
+class TestRoute(Route):
+
+    def handle(self):
+        print(self.request.method)
+        print(self.request.uri)
+        print(self.request.protocol)
+        print(self.request.headers)
+        print(self.request.body)
+        data = {
+            "message": "successful"
+        }
+        self.response.send(200, json.dumps(data), content_type="application/json")
+
+
+class EchoWebsocketRoute(WebSocketRoute):
+
+    def handle(self):
+        while True:
+            received_message = self._recv()
+            print("Received Text: ", received_message)
+            if received_message is None:
+                break
+            else:
+                self.response.send("Good!")
---
+++ src/server/server.py
@@ -0,0 +1,157 @@
+import socket
+import hashlib
+import base64
+import struct
+import threading
+import re
+
+from route import TestRoute, Request, Response, EchoWebsocketRoute, WebSocketResponse, HttpResponse
+
+# Define WebSocket server information
+HOST = 'localhost'
+PORT = 8766
+
+class ProtocolTypeRouter(object):
+
+    def __init__(self, protocols, bind='localhost', port=8888):
+        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        self._server_socket.bind((bind, port))
+        self._server_socket.listen(5)
+
+        self._protocols = protocols
+        pass
+
+    def handle(self, client_socket):
+        pass
+
+
+    def routing(self, client_socket):
+        # if protocol is http
+        request = Request(client_socket)
+        if 'Upgrade' in request.headers and request.headers['Upgrade'] == 'websocket':
+            response = WebSocketResponse(client_socket)            
+            self._protocols['websocket'].handle(request, response)
+        else:
+            response = HttpResponse(client_socket)
+            self._protocols['http'].handle(request, response) 
+
+        # if protocol is websocket
+        # self._protocols['websocket'].handle(client_socket)
+        pass
+
+    def run(self):
+        try:
+            while True:
+                client_socket, addr = self._server_socket.accept()
+                client_handler = threading.Thread(target=self.routing, args=(client_socket,))
+                client_handler.start()
+        except KeyboardInterrupt:
+            self._server_socket.close()
+
+
+class Server(object):
+
+    def __init__(self, routes=[]):
+        self._routes = routes
+        pass
+
+    def handle(self):
+        pass
+
+
+class HttpServer(Server):
+    
+    def __init__(self, routes=[]):
+        super().__init__(routes)
+        pass
+
+
+    def handle(self, request:Request, response:Response):
+        # get uri of the request
+        uri = request.uri
+        for i in range(len(self._routes)):
+            reg = re.compile(self._routes[i][0])
+            if reg.match(uri):
+                route = self._routes[i][1](request, response)
+                route.handle()
+                break
+
+class WebSocketServer(Server):
+
+    def __init__(self, routes=[]):
+        super().__init__(routes)
+        pass
+
+    def _create_handshake_response(self, request:Request):
+        key =  request.headers['Sec-WebSocket-Key'].encode('utf-8')
+        guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+        response_key = base64.b64encode(hashlib.sha1(key + guid.encode('utf-8')).digest()).decode('utf-8')
+    
+        response = "HTTP/1.1 101 Switching Protocols\r\n"
+        response += "Upgrade: websocket\r\n"
+        response += "Connection: Upgrade\r\n"
+        response += f"Sec-WebSocket-Accept: {response_key}\r\n\r\n"
+    
+        return response
+    
+    def decode_websocket_data(self, data):
+        opcode = data[0] & 0x0F
+        payload_length = data[1] & 0x7F
+        mask_key = data[2:6]
+        encoded_data = data[6:]
+
+        decoded_data = bytearray(len(encoded_data))
+        for i in range(len(encoded_data)):
+            decoded_data[i] = encoded_data[i] ^ mask_key[i % 4]
+
+        return opcode, decoded_data
+
+    def handle(self, request:Request, response:Response):
+                # routing to websocket route handler
+
+        # get uri of the request
+        match = False
+        uri = request.uri
+        for i in range(len(self._routes)):
+            reg = re.compile(self._routes[i][0])
+            if reg.match(uri):
+                match = True
+                handshake_response = self._create_handshake_response(request)
+                response.socket().send(handshake_response.encode('utf-8'))
+
+                route = self._routes[i][1](request, response)
+                route.handle()
+                break
+        if match == False:
+            response.close()
+
+
+
+
+
+
+
+# Handle WebSocket connections
+
+
+# Create a socket server
+# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server_socket.bind((HOST, PORT))
+# server_socket.listen(5)
+# 
+# print(f"WebSocket server is listening on ws://{HOST}:{PORT}")
+# 
+# while True:
+#     client_socket, addr = server_socket.accept()
+#     client_handler = threading.Thread(target=handle_websocket, args=(client_socket,))
+#     client_handler.start()
+
+
+if __name__ == '__main__':
+    server = ProtocolTypeRouter({
+        'http': HttpServer(routes=[(r'/', TestRoute)]),
+        'websocket': WebSocketServer(routes=[(r'/chat', EchoWebsocketRoute)])
+    })
+    server.run()
+    pass
